3574545fb29d1387c7f67bdd2c8322f002cfb073
ChengCK18 <chinkheng_98@hotmail.com>
2023-11-19 23:34:12 +0800

    golang, post request to mongodb successful

--- go-prog-backend/pkg/database/mongodb.go
+++ go-prog-backend/pkg/database/mongodb.go
@@ -1,59 +1,68 @@
-// package database
+package database
 
-// import (
-// 	"context"
-// 	"log"
-// 	"github.com/joho/godotenv"
-// 	"go.mongodb.org/mongo-driver/mongo"
-// 	"go.mongodb.org/mongo-driver/mongo/options"
-// )
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
 
-// var client *mongo.Client
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
-// func init() {
+type SampleData struct {
+	Name string // need to be caps to be exported
+	Age  int    // need to be caps to be exported
+}
 
-// 	err := godotenv.Load()
-// 	if err != nil {
-// 		fmt.Println("Unable to load relevant env file for MongoDB cluster connection")
-// 	}
+var client *mongo.Client
 
-// 	// Get environment variables
-// 	username := os.Getenv("MONGODB_USERNAME")
-// 	password := os.Getenv("MONGODB_PASS")
-// 	clusterAddress := os.Getenv("MONGODB_CLUSTER_ADDRESS")
+func init() {
 
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		fmt.Println("Unable to load relevant env file for MongoDB cluster connection")
+		fmt.Println(err)
+		return
+	}
 
-// 	// Set up MongoDB connection
-// 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-// 	clientOptions := options.Client().ApplyURI("mongodb+srv://" + username + ":" + password + "@" + clusterAddress + "/testgolang?retryWrites=true&w=majority").SetServerAPIOptions(serverAPI)
-// 	var err error
-// 	client, err = mongo.Connect(context.TODO(), clientOptions)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+	// Get environment variables
+	username := os.Getenv("MONGODB_USERNAME")
+	password := os.Getenv("MONGODB_PASS")
+	clusterAddress := os.Getenv("MONGODB_CLUSTER_ADDRESS")
 
+	// Set up MongoDB connection
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	clientOptions := options.Client().ApplyURI("mongodb+srv://" + username + ":" + password + "@" + clusterAddress + "/testgolang?retryWrites=true&w=majority").SetServerAPIOptions(serverAPI)
 
-// 	// Perform a ping to check if the credentials are correct and connection successfully established for operations
-// 	err = client.Ping(context.TODO(), nil)
-// 	if err != nil {
-// 		// Handle authentication or other specific connection errors
-// 		fmt.Println("Authentication failed. Please chck credentials.")
-// 		return
-// 	}
-// 	fmt.Println("Connection to MongoDB successful.")
+	client, err = mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	defer func() {
-// 		if err = client.Disconnect(context.TODO()); err != nil {
-// 			panic(err)
-// 		}
-// 	}()
-// }
+	// Perform a ping to check if the credentials are correct and connection successfully established for operations
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		// Handle authentication or other specific connection errors
+		fmt.Println("Authentication failed. Please chck credentials.")
+		return
+	}
+	fmt.Println("Connection to MongoDB successful.")
 
+	// if put defer here, it will cause the client to disconnect upon completion of init func
+	// defer func() {
+	// 	if err = client.Disconnect(context.TODO()); err != nil {
+	// 		panic(err)
+	// 	}
+	// }()
+}
 
-// func InsertIntoMongoDB(data userData) error {
-// 	collection := client.Database("testgolang").Collection("golangdb1")
+func InsertIntoMongoDB(data SampleData) error {
+	collection := client.Database("testgolang").Collection("golangdb1")
 
-// 	// Insert data into MongoDB
-// 	_, err := collection.InsertOne(context.TODO(), data)
-// 	return err
-// }
+	// Insert data into MongoDB
+	_, err := collection.InsertOne(context.TODO(), data)
+	fmt.Println("geeee", err)
+	return err
+}
--- go-prog-backend/pkg/handlers/handlers.go
+++ go-prog-backend/pkg/handlers/handlers.go
@@ -1,11 +1,12 @@
 package handlers
 
 import (
-	"net/http"
 	"encoding/json"
+	"fmt"
+	"net/http"
 
+	"github.com/ChengCK18/go-proj-backend/pkg/database"
 )
-
 
 type Response struct {
 	Message string
@@ -13,24 +14,43 @@ type Response struct {
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 
-    var response Response
+	var response Response
 
-    switch r.Method {
-		case http.MethodGet:
-			response = Response{Message: "Hello from Houston, hearing you loud and clear (GET)"}
-		case http.MethodPost:
-			response = Response{Message: "Hello from Houston, receiving your POST request"}
-		case http.MethodPut:
-			response = Response{Message: "Hello from Houston, processing your PUT request"}
-		case http.MethodDelete:
-			response = Response{Message: "Hello from Houston, handling your DELETE request"}
-		default:
-			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-			return
-   	}
-
+	switch r.Method {
+	case http.MethodGet:
+		response = Response{Message: "Hello from Houston, hearing you loud and clear (GET)"}
+	case http.MethodPost:
+		HelloHandlerPost(w, r)
+	case http.MethodPut:
+		response = Response{Message: "Hello from Houston, processing your PUT request"}
+	case http.MethodDelete:
+		response = Response{Message: "Hello from Houston, handling your DELETE request"}
+	default:
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
 	// response := Response{Message: "Hello from Houston, hearing you loud and clear "+r.Method}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
+}
+
+func HelloHandlerPost(w http.ResponseWriter, r *http.Request) {
+	var userInput database.SampleData
+
+	err := json.NewDecoder(r.Body).Decode(&userInput)
+	if err != nil {
+		http.Error(w, "Invalid JSON data, missing name and/or age field", http.StatusBadRequest)
+		return
+	}
+
+	err2 := database.InsertIntoMongoDB(userInput)
+	if err2 != nil {
+		http.Error(w, "Failed to insert data into MongoDB", http.StatusInternalServerError)
+		return
+	}
+
+	// Respond with a success message or appropriate response
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "Data inserted successfully")
 }
